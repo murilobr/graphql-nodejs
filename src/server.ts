29fd25e4078462dbe6509175b7ee17fd72505e26
Murilo da Silva <murilobr@gmail.com>
2021-11-23 19:58:17 -0300

    feat: remove commented code

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,3 @@
-// import { buildSchema, graphql } from "graphql";
-
-// const schema = buildSchema(`
-// 	type Query {
-// 		hello: String
-// 	}`);
-
-// const root = {
-//     hello: () => "Hello World!",
-// };
-
-// const query = `{
-// 	hello
-// }`;
-
-// graphql({ schema, rootValue: root, source: query }).then((result) => {
-//     console.log(JSON.stringify(result, null, 4));
-// });
-
 import { buildSchema } from "graphql";
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
