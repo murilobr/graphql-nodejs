3b139b0cc6df9fd422efd3298bc0bd092da438e5
Murilo da Silva <murilobr@gmail.com>
2021-11-23 17:35:14 -0300

    feat: implements graphql

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-console.log("it works");
+import { buildSchema, graphql } from "graphql";
+
+const schema = buildSchema(`
+	type Query {
+		hello: String
+	}`);
+
+const root = {
+    hello: () => "Hello World!",
+};
+
+const query = `{
+	hello
+}`;
+
+graphql({ schema, rootValue: root, source: query }).then((result) => {
+    console.log(JSON.stringify(result, null, 4));
+});
