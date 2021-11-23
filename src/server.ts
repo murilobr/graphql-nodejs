c5780d7bb482e5521b379ba01d9a69e2cc061c3e
Murilo da Silva <murilobr@gmail.com>
2021-11-23 19:56:56 -0300

    feat: implements express graphql server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,27 @@
-import { buildSchema, graphql } from "graphql";
+// import { buildSchema, graphql } from "graphql";
+
+// const schema = buildSchema(`
+// 	type Query {
+// 		hello: String
+// 	}`);
+
+// const root = {
+//     hello: () => "Hello World!",
+// };
+
+// const query = `{
+// 	hello
+// }`;
+
+// graphql({ schema, rootValue: root, source: query }).then((result) => {
+//     console.log(JSON.stringify(result, null, 4));
+// });
+
+import { buildSchema } from "graphql";
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+
+const app = express();
 
 const schema = buildSchema(`
 	type Query {
@@ -9,10 +32,9 @@ const root = {
     hello: () => "Hello World!",
 };
 
-const query = `{
-	hello
-}`;
+app.use(
+    "/graphql",
+    graphqlHTTP({ schema: schema, rootValue: root, graphiql: true })
+);
 
-graphql({ schema, rootValue: root, source: query }).then((result) => {
-    console.log(JSON.stringify(result, null, 4));
-});
+app.listen(4000);
